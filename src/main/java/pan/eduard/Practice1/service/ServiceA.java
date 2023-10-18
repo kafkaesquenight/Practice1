aaaa8bfcfa33438d250869abdbed655bc9890118
kafkaesquenight <eddiebang06@gmail.com>
2023-10-18 10:21:44 +0600

    redone

--- src/main/java/pan/eduard/Practice1/service/ServiceA.java
+++ src/main/java/pan/eduard/Practice1/service/ServiceA.java
@@ -5,17 +5,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import pan.eduard.Practice1.repository.RepoA;
 import org.springframework.stereotype.Service;
-
-
 @Service
 public class ServiceA {
     Logger log = Logger.getLogger(ServiceA.class.getName());
+    @Value("${attribute.value:this is default}")
+    private String attribute;
     private RepoA repoA;
     @Value("class {ServiceA} method called")
     private String serviceAMsg;
     public void methodServiceA(){
         log.info(serviceAMsg);
         repoA.methodRepoA();
+    }
+
+    public String getAttribute() {
+        return attribute;
+    }
+
+    public void setAttribute(String attribute) {
+        this.attribute = attribute;
     }
     public ServiceA(){
         log.info(serviceAMsg);
