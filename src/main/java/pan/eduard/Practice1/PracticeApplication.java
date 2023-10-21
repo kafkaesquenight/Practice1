6a84fafeb5630228d864e431783ce74f495ab401
kafkaesquenight <eddiebang06@gmail.com>
2023-10-21 21:10:44 +0600

    redone

--- src/main/java/pan/eduard/Practice1/PracticeApplication.java
+++ src/main/java/pan/eduard/Practice1/PracticeApplication.java
@@ -1,19 +1,17 @@
 package pan.eduard.Practice1;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 
-import java.util.logging.Logger;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import pan.eduard.Practice1.service.ServiceA;
-
 @SpringBootApplication
+@Slf4j
 public class PracticeApplication {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext ctx = SpringApplication.run(PracticeApplication.class, args);
-		Logger log = Logger.getLogger(ServiceA.class.getName());
 
 	}
 
