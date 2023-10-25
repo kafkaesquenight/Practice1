5646ff557a166b7abb35a9211748a3d3fed9eb86
kafkaesquenight <eddiebang06@gmail.com>
2023-10-25 22:53:16 +0600

    redone

--- src/main/java/pan/eduard/Practice1/MyAspect.java
+++ src/main/java/pan/eduard/Practice1/MyAspect.java
@@ -4,6 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
 @Aspect
 @Component
 @Slf4j
@@ -11,30 +15,22 @@ public class MyAspect {
     @Before("execution(* pan.eduard.Practice1.service.ServiceD.*(..))")
     public void logBefore(JoinPoint joinPoint) {
         log.info("logBefore called. Method name: " + joinPoint.getSignature().toShortString());
-
     }
-    @After("execution(* pan.eduard.Practice1.service.ServiceD.*(..))")
+    @AfterThrowing("execution(* pan.eduard.Practice1.service..*(..))")
+    public void logAfterThrow(JoinPoint joinPoint){
+        log.info("logAfterThrowing called. Method name: " + joinPoint.getSignature().toShortString());
+    }
+    @AfterReturning("execution(* pan.eduard.Practice1.service..*(..))")
+    public void logAfterReturn(JoinPoint joinPoint){
+        log.info("AfterReturning. Method name: " + joinPoint.getSignature().toShortString());
+    }
+
+    @After("execution(* pan.eduard.Practice1.service..*(..))")
     public void logAfter(JoinPoint joinPoint){
-        log.info("After called. Method name: " + joinPoint.getSignature().toShortString());
+        log.info("logAfter called. Method name: " + joinPoint.getSignature().toShortString());
     }
     @Pointcut("execution(* pan.eduard.Practice1.service..*(..))")
     protected void loggingOperation(){
 
     }
-    @Around("execution(* pan.eduard.Practice1.service.ServiceD.*(..))")
-    public void logAround() {
-        log.info("Around method getting called");
-    }
-
-    @AfterReturning("execution(* pan.eduard.Practice1.service..*(..))")
-    public void afterReturning(JoinPoint joinPoint){
-        log.info("AfterReturning. Method name: " + joinPoint.getSignature().toShortString());
-    }
-
-    @AfterThrowing("execution(* pan.eduard.Practice1.service.ServiceC*.*(..))")
-    public void AfterReturning(JoinPoint joinPoint){
-        log.info("AfterReturning. Method name: " + joinPoint.getSignature().toShortString());
-    }
-
-
 }
--- src/main/java/pan/eduard/Practice1/MyConfiguration.java
+++ src/main/java/pan/eduard/Practice1/MyConfiguration.java
@@ -1,7 +1,8 @@
 package pan.eduard.Practice1;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
 import pan.eduard.Practice1.service.ServiceA;
 import pan.eduard.Practice1.service.ServiceB;
 import pan.eduard.Practice1.service.ServiceC;
@@ -11,15 +12,21 @@ import pan.eduard.Practice1.repository.RepoB;
 import pan.eduard.Practice1.repository.RepoC;
 import org.springframework.context.annotation.Bean;
 @Configuration
+@PropertySource(value = "classpath:application.properties")
 public class MyConfiguration {
+    @Value("${value.from.application:default}:")
+    private String value;
     @Bean("serviceAFromConfiguration")
-    @Primary
     public ServiceA getServiceA(){
         ServiceA serviceA = new ServiceA(new RepoA());
-        serviceA.setAttribute("FIRST attribute from Configuration class");
         return serviceA;
     }
 
+    @Bean("serviceBFromConfiguration")
+    public ServiceB getServiceB(){
+        ServiceB serviceB = new ServiceB(new ServiceA(new RepoA()), new RepoB());
+        return serviceB;
+    }
 
     @Bean("serviceCFromConfiguration")
     public ServiceC getServiceC(){
